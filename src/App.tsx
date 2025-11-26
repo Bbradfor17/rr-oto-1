ec2f92985f8d733893e14a1a59046adb14754c0a
Ben Byrd <bbyrd@inmarket.com>
2025-11-26 12:39:42 -0500

    fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { theme } from "./theme";
 import { EbookPage, OTO1 } from "./pages";
 
-const BASENAME = "/revolve-ebook-offer";
-
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Router basename={BASENAME}>
+      <Router basename="/revolve-ebook-offer">
         <Routes>
           <Route path="/" element={<EbookPage />} />
           <Route path="/oto" element={<OTO1 />} />
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-
   const apiRoot = env.API_ROOT;
   const useMockData = env.USE_MOCK_DATA;
   const recaptchaKey = env.RECAPTCHA_API_KEY;
 
   return {
-    base: "/revolve-ebook-offer",
+    base: "/revolve-ebook-offer/",
     plugins: [react()],
     define: {
       "process.env.API_ROOT": apiRoot ? JSON.stringify(apiRoot) : "undefined",
